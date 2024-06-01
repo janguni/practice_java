291ea53054e2c9f0ceb18d4966da587e8e2c1fb9
janguni <lightson23@naver.com>
2024-06-01 16:49:57 +0900

    [3. 연산자] 논리연산 시 주의할 점

---
+++ src/main/java/연산자_03/논리연산자.java
@@ -0,0 +1,10 @@
+package 연산자_03;
+
+public class 논리연산자 {
+
+	public static void main(String[] args) {
+
+		// ||(or)를 사용하는 경우 왼쪽 연산결과가 true라면 오른쪽 연산결과를 확인하지 않는다.
+		// 그래서 연산결과가 '참'일 확률이 높은 연산을 왼쪽에 넣는 것이 효율적이다.
+	}
+}
